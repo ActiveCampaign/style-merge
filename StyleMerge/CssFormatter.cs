1565f868f6afb916ab46eba58383c694e03bae8a
Ryan Criddle <ryan.criddle@wildbit.com>
2022-06-29 14:47:05 -0700

    Improve transparent colour handling ... yet again

--- StyleMerge/CssFormatter.cs
+++ StyleMerge/CssFormatter.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 using AngleSharp;
-using AngleSharp.Css.Dom;
-using AngleSharp.Css.Values;
 using AngleSharp.Text;
 
 namespace StyleMerge
 {
     public class CssFormatter : IStyleFormatter
     {
-        private const string RgbaPattern = @"rgba[(](\d{1,3})\s?,\s?(\d{1,3})\s?,\s?(\d{1,3}),\s?.*[)]";
+        private const string RgbaPattern = @"rgba[(](\d{1,3})\s?,\s?(\d{1,3})\s?,\s?(\d{1,3}),\s?(.*)[)]";
         private static readonly MatchEvaluator RgbaEvaluator = new MatchEvaluator(ReplaceRgba);
 
         public string Declaration(string name, string value, bool important)
@@ -33,7 +30,7 @@ namespace StyleMerge
 
             using (var writer = new StringWriter(sb))
             {
-                foreach (var declaration in Filter(declarations))
+                foreach (var declaration in declarations)
                 {
                     writer.Write(Symbols.Space);
                     declaration.ToCss(writer, this);
@@ -58,7 +55,7 @@ namespace StyleMerge
 
             using (var writer = new StringWriter(sb))
             {
-                foreach (var rule in Filter(rules))
+                foreach (var rule in rules)
                 {
                     writer.Write(Symbols.Space);
                     rule.ToCss(writer, this);
@@ -92,7 +89,7 @@ namespace StyleMerge
 
             using (var writer = new StringWriter(sb))
             {
-                foreach (var rule in Filter(rules))
+                foreach (var rule in rules)
                 {
                     rule.ToCss(writer, this);
                     writer.Write(sep);
@@ -109,24 +106,16 @@ namespace StyleMerge
 
         private static string ReplaceRgba(Match match)
         {
+            // If alpha channel is empty (ie. background: transparent) there is no compatible 6-digit
+            // hex representation ... we need to return 'transparent' in this special case.
+            if (float.Parse(match.Groups[4].Value) == 0)
+                return "transparent";
+
             var r = byte.Parse(match.Groups[1].Value);
             var g = byte.Parse(match.Groups[2].Value);
             var b = byte.Parse(match.Groups[3].Value);
             
             return $"#{r:X2}{g:X2}{b:X2}";
-        }
-
-        private IEnumerable<IStyleFormattable> Filter(IEnumerable<IStyleFormattable> rules)
-            => rules.Where(x => !IsTransparentColor(x));
-
-        private static bool IsTransparentColor(IStyleFormattable rule)
-        {
-            // If alpha channel is completely transparent (ie. background: transparent), we don't render
-            // this rule since there is no compatible 6-digit hex representation ... including this rule
-            // *should* not be necessary anyway, it is probably redundant in most cases.
-            return rule is ICssProperty prop
-                && prop.RawValue is Color color
-                && color.Alpha == 0;
         }
     }
 }
